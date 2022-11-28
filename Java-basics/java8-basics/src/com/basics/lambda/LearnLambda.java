1137e39fe5f0b4a24302d704c5c49c1e2aa4b49c
unknown <cogjava4584@iiht.tech>
2022-11-28 15:34:20 +0530

    Normal implementation and with lambda expression

--- Java-basics/java8-basics/src/com/basics/lambda/LearnLambda.java
+++ Java-basics/java8-basics/src/com/basics/lambda/LearnLambda.java
@@ -1,11 +1,35 @@
 package com.basics.lambda;
 
+interface SayHello{
+	public void hello();
+}
+
 public class LearnLambda {
 
 	// parameter -> expression body
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		// Normal implementation of SayHello interface
+		
+		SayHello h1 = new SayHello() {
+			
+			@Override
+			public void hello() {
+				System.out.println("Normal implementation of SayHello interface");
+			}
+		};
+		
+		h1.hello();
+		
+		// Implementation of SayHello interface using Lambda
+		
+		SayHello h2 = () -> {
+			System.out.println("Implementation of SayHello interface using Lambda");
+		};
+		
+		h2.hello();
 
 		MathOperation add = (Integer num1, Integer num2) -> num1 + num2;
 
