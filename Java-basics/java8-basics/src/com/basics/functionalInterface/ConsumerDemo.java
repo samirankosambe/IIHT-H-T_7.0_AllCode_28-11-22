08d0aac2d20c1c8932c9ee4ee2b11d7622d2e98b
unknown <cogjava4584@iiht.tech>
2022-11-29 14:49:28 +0530

    Consumer interface with for each

--- Java-basics/java8-basics/src/com/basics/functionalInterface/ConsumerDemo.java
+++ Java-basics/java8-basics/src/com/basics/functionalInterface/ConsumerDemo.java
@@ -1,5 +1,7 @@
 package com.basics.functionalInterface;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumerDemo {
@@ -7,6 +9,10 @@ public class ConsumerDemo {
 	public static void main(String[] args) {
 		Consumer<Integer> consumer = (t) -> System.out.println("Printing : " + t);
 		consumer.accept(100);
+
+		List<Integer> numberList = Arrays.asList(1, 2, 3, 4, 5);
+		numberList.forEach(consumer);
+		numberList.forEach(number -> System.out.println("Printing : " + number));
 	}
 
 }
