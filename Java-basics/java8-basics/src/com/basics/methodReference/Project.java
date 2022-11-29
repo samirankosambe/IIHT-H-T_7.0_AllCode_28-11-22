00df76b3962aa0601e449b0791a32d719bc0d6a5
unknown <cogjava4584@iiht.tech>
2022-11-29 16:26:37 +0530

    Method Reference added outputs

--- Java-basics/java8-basics/src/com/basics/methodReference/Project.java
+++ Java-basics/java8-basics/src/com/basics/methodReference/Project.java
@@ -1,7 +1,12 @@
 package com.basics.methodReference;
 
+import java.time.LocalDate;
+
 public class Project {
 	public static void projectProgress() {
 		System.out.println("Project is making progress...");
+		System.out.println("The status is approved by client");
+		LocalDate date = LocalDate.now();
+		System.out.println("On: " + date.toString());
 	}
 }
