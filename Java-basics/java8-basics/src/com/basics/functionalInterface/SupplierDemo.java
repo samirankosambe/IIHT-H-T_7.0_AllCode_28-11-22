36095dcd13f7112499dc02f280682cb1190bb551
unknown <cogjava4584@iiht.tech>
2022-11-29 15:52:38 +0530

    Supplier interace with lambda expression

--- Java-basics/java8-basics/src/com/basics/functionalInterface/SupplierDemo.java
+++ Java-basics/java8-basics/src/com/basics/functionalInterface/SupplierDemo.java
@@ -2,16 +2,12 @@ package com.basics.functionalInterface;
 
 import java.util.function.Supplier;
 
-public class SupplierDemo implements Supplier<String> {
+public class SupplierDemo{
 
 	public static void main(String[] args) {
-		Supplier<String> supplier = new SupplierDemo();
+		Supplier<String> supplier = () -> {return "Hello World";};
 		System.out.println(supplier.get());
 	}
 
-	@Override
-	public String get() {
-		return "Hello World";
-	}
 
 }
