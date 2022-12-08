6fcf4788193823865bf71619659fb4a1692c139b
unknown <cogjava4584@iiht.tech>
2022-12-08 18:42:00 +0530

    Use case 1 added name and rest template

--- Use-Case 1/Book-Service/src/main/java/com/book/BookServiceApplication.java
+++ Use-Case 1/Book-Service/src/main/java/com/book/BookServiceApplication.java
@@ -2,6 +2,8 @@ package com.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BookServiceApplication {
@@ -9,5 +11,10 @@ public class BookServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookServiceApplication.class, args);
 	}
-
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
 }
