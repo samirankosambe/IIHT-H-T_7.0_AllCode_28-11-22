0dc411df25aa9958b1f74c9e6997e68109bf5758
unknown <cogjava4584@iiht.tech>
2022-12-08 15:05:42 +0530

    linking hr to resource

--- server-side/HR-Service/src/main/java/com/hr/controller/HRController.java
+++ server-side/HR-Service/src/main/java/com/hr/controller/HRController.java
@@ -1,12 +1,16 @@
 package com.hr.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.hr.entity.HR;
+import com.hr.entity.Resource;
 import com.hr.service.IHrService;
 
 @RestController
@@ -15,9 +19,15 @@ public class HRController {
 
 	@Autowired
 	IHrService hrService;
+	
+	@Autowired
+	RestTemplate restTemplate;
 
 	@GetMapping("/{userID}")
 	public HR getHr(@PathVariable Long userID) {
-		return hrService.getHr(userID);
+		HR hr = hrService.getHr(userID);
+		List<Resource> resources = this.restTemplate.getForObject("http://localhost:5002/resource/hr/"+userID, List.class);
+		hr.setResourses(resources);
+		return hr;
 	}
 }
