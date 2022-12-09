23f56c6e699b3f60ab0fa3b8c3810939a691051e
unknown <cogjava4584@iiht.tech>
2022-12-09 15:18:35 +0530

    Spring security working

---
+++ server-side/JWT-Demo/src/main/java/com/token/controller/SecurityController.java
@@ -0,0 +1,13 @@
+package com.token.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+
+	@GetMapping("/")
+	public String home() {
+		return "Hello and Welcome to home page";
+	}
+}
