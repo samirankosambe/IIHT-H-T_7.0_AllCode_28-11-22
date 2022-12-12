4b0f0bf62fe9377be2c4c900b7fcb3cd0c567198
unknown <cogjava4584@iiht.tech>
2022-12-12 15:24:37 +0530

    Spring security added security filter

--- server-side/JWT-Demo/src/main/java/com/token/config/SecurityConfiguration.java
+++ server-side/JWT-Demo/src/main/java/com/token/config/SecurityConfiguration.java
@@ -8,22 +8,28 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.token.filter.JWTFilter;
 import com.token.service.UserService;
 
 @SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
+	@Autowired
+	private JWTFilter jwtFilter;
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userService);
 	}
-	
+
 	@Override
 	@Bean
 	public AuthenticationManager authenticationManager() throws Exception {
@@ -32,6 +38,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers("/auth").permitAll().anyRequest().authenticated();
+		http.csrf()
+		.disable()
+		.authorizeRequests()
+		.antMatchers("/auth")
+		.permitAll()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 }
