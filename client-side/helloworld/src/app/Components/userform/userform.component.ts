a62e7333adf53e68036cb507069e760210a88dac
unknown <cogjava4584@iiht.tech>
2022-12-14 17:48:12 +0530

    Angular binding

--- client-side/helloworld/src/app/Components/userform/userform.component.ts
+++ client-side/helloworld/src/app/Components/userform/userform.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserformComponent implements OnInit {
 
+  title:String = "Please fill the form below";
+
+  firstname:String = "";
+  lastname:String = "";
+  age=0;
+  gender:String = "male";
+  email:String="";
+
+  save(){
+    console.log("Hello "+this.firstname+" "+this.lastname);
+    console.log("your email address is "+this.email); 
+  }
   constructor() { }
 
   ngOnInit(): void {
