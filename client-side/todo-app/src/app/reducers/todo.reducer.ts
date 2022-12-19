cea43d56ab02062d5eb19a384e8d5d63cf2d25ed
unknown <cogjava4584@iiht.tech>
2022-12-19 15:14:55 +0530

    Angular added action, store and reducer

---
+++ client-side/todo-app/src/app/reducers/todo.reducer.ts
@@ -0,0 +1,14 @@
+import { ActionParent } from "../actions/todo.action";
+import { Todo } from "../models/Todo";
+
+const initialState: Todo [] = [
+    {title: "title 1"},
+    {title: "title 2"},
+    {title: "title 3"}
+];
+
+export function Todoreducer(state = initialState, action: ActionParent) {
+    switch(action) {
+        default: return state;
+    }
+}
