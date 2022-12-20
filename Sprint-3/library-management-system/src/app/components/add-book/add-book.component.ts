4087de0a52d2520d6b7c8a703e02f9721386b2ba
unknown <cogjava4584@iiht.tech>
2022-12-20 16:21:47 +0530

    Sprint 3 added styling

--- Sprint-3/library-management-system/src/app/components/add-book/add-book.component.ts
+++ Sprint-3/library-management-system/src/app/components/add-book/add-book.component.ts
@@ -13,29 +13,38 @@ export class AddBookComponent implements OnInit {
 
   book: Book = new Book();
 
+  validated: boolean = true;
+
   save(){
     if(this.book.name == ""){
+      this.validated = false;
       alert("Please add book name")
     }
 
     if(this.book.author == ""){
+      this.validated = false;
       alert("Please add book author")
     }
     if(isNaN(this.book.price) || this.book.price <= 0 ){
+      this.validated = false;
       alert("Please add book price")
     }
     if(this.book.genre == ""){
+      this.validated = false;
       alert("Please add book genre")
     }
-    const observables = this.bookService.saveBook(this.book);
-    observables.subscribe(
-      (response: any) => {
-        console.log(response);
-      }, function (error) {
-        console.log(error);
-        alert("Something went wrong, please check again!");
-      }
-    )
+    if(this.validated = true){
+      const observables = this.bookService.saveBook(this.book);
+      observables.subscribe(
+        (response: any) => {
+          console.log(response);
+        }, function (error) {
+          console.log(error);
+          alert("Something went wrong, please check again!");
+        }
+      )
+    }
+    
   }
 
   constructor(private bookService: BookService) { }
